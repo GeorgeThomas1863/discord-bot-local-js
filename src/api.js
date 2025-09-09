bf65015fc1cd151f59ede68c3a27574d734b1ce5
GeorgeThomas1863 <georgebushdontcare@gmail.com>
2025-09-09 09:46:04 -0400

    dev

--- src/api.js
+++ src/api.js
@@ -1,7 +1,13 @@
+import { OpenAI } from "openai";
+import { OPENAI_KEY } from "../config/bot.js";
+
+//NOT DONE
 const openai = new OpenAI({
+  baseURL: "http://127.0.0.1:1234",
   apiKey: OPENAI_KEY,
 });
 
+//SET CORRECT ENDPOINT HERE
 export const sendToLocalAPI = async (inputArray) => {
   const params = {
     model: "gpt-4",
@@ -10,11 +16,11 @@ export const sendToLocalAPI = async (inputArray) => {
 
   try {
     const res = await openai.chat.completions.create(params);
-    const aiMessage = res.choices[0].message.content;
+    const messageLLM = res.choices[0].message.content;
     // console.log("AI MESSAGE");
     // console.log(aiMessage);
 
-    return aiMessage;
+    return messageLLM;
   } catch (e) {
     console.error("OpenAI API Error:", e.message);
     if (e.status === 429) return "SAM ALTMAN WANT HIS MONEY (George didn't pay his API bill)";
