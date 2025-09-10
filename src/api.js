e346c84eb34ef9280518fb46c3a12d83322067f9
GeorgeThomas1863 <georgebushdontcare@gmail.com>
2025-09-09 21:15:08 -0400

    dev

--- src/api.js
+++ src/api.js
@@ -36,10 +36,16 @@ export const sendToLocalAPI = async (inputArray) => {
 
 export const defineSystemPrompt = async () => {
   const systemPrompt = [
+    // {
+    //   role: "system",
+    //   content:
+    //     "You are a helpful, friendly, and clever assistant. Please provide a response to the following message or messages. Please review all of the content provided and respond with a detailed and CONCISE reply.",
+    // },
+
     {
       role: "system",
       content:
-        "You are a helpful, friendly, and clever assistant. Please provide a response to the following message or messages. Please review all of the content provided and respond with a detailed and CONCISE reply.",
+        "You are a helpful, friendly, and clever assistant. You are in a heated argument with the user. The user is on Team A, you are on Team B. Please argue from Team B's point of view. Be sure to respond to whatever the user is claiming or arguing, be passionate and convincing, but be very concise and to the point.",
     },
   ];
 
--- src/discord-bot.js
+++ src/discord-bot.js
@@ -5,16 +5,19 @@ import { startTyping, stopTyping, fixUsername } from "./util.js";
 export const handleMessage = async (inputObj, client) => {
   const { channel } = inputObj;
 
-  // console.log("INPUT OBJECT");
-  // console.log(inputObj);
+  console.log("INPUT OBJECT");
+  console.log(inputObj);
 
   const msgIgnore = await checkMsgIgnore(inputObj, client);
-  if (msgIgnore) return null;
+  if (!msgIgnore) return null;
 
   const typingInterval = startTyping(channel);
 
   try {
     const convoArray = await buildConvoArray(channel, client);
+    console.log("CONVO ARRAY");
+    console.log(convoArray);
+
     const msgLLM = await sendToLocalAPI(convoArray);
 
     await sendMsgChunk(msgLLM, inputObj);
@@ -28,11 +31,11 @@ export const handleMessage = async (inputObj, client) => {
 
 //check if message should be ignored
 export const checkMsgIgnore = async (inputObj, client) => {
-  const { author, content, channelId, mentions } = inputObj;
+  const { content, channelId, mentions } = inputObj;
   const { CHANNELS, PREFIX2 } = CONFIG;
 
   if (!CHANNELS.includes(channelId)) return null;
-  if (author.bot) return null;
+  // if (author.bot) return null;
 
   const firstChar = content.trim().charAt(0);
   const botMention = mentions.users.has(client.user.id) || null;
@@ -43,7 +46,7 @@ export const checkMsgIgnore = async (inputObj, client) => {
 };
 
 export const buildConvoArray = async (channel, client) => {
-  // const { PREFIX2 } = CONFIG;
+  const { PREFIX2 } = CONFIG;
 
   //set system prompt as first msg in array
   const convoArray = await defineSystemPrompt();
@@ -57,13 +60,14 @@ export const buildConvoArray = async (channel, client) => {
 
     //time check
     const timeCheck = Date.now() - 15 * 60 * 1000; // 15 min ago
+    // const timeCheck = Date.now() - 1 * 60 * 1000; // 1 min ago
     if (createdTimestamp < timeCheck) continue;
 
     // console.log("MESSAGE OBJECT");
     // console.log(messageObj);
 
     //CHECK THIS IS RIGHT
-    // if (author.id !== client.user.id && !content.startsWith(PREFIX2)) continue;
+    if (author.id !== client.user.id && !content.startsWith(PREFIX2)) continue;
 
     const username = await fixUsername(author.username);
 
@@ -87,6 +91,6 @@ export const sendMsgChunk = async (msg, inputObj) => {
 
   for (let i = 0; i < msg.length; i += CHUNK_SIZE_LIMIT) {
     const chunk = msg.substring(i, i + CHUNK_SIZE_LIMIT);
-    await inputObj.reply(chunk);
+    await inputObj.reply("!" + chunk);
   }
 };
